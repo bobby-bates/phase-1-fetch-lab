8f17b14b23f0e517de1f6f5296965abf76acdde3
Bobby Bates <robertfbatesjr@gmail.com>
2022-05-08 15:24:50 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  const booksFetch = fetch('https://anapioficeandfire.com/api/books')
+    .then(res => res.json())
+    .then(json => renderBooks(json))
+    return booksFetch
 }
 
 function renderBooks(books) {
